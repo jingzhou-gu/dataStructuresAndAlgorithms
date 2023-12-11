be99ccdd259768ebe77fc5a66b23e2742893e9ba
gjz <gjz960420@163.com>
2023-12-11 21:48:11 +0800

    完善二分查找算法

--- src/main/java/search/BinarySearch.java
+++ src/main/java/search/BinarySearch.java
@@ -1,5 +1,7 @@
 package search;
 
+import java.util.ArrayList;
+
 /**
  * @Author: gjz
  * @Date: 2023/12/11 18:17
@@ -9,6 +11,8 @@ public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {-3, 2, 5, 5, 13, 14, 14, 46};
         System.out.println(binarySearch(arr, 0, arr.length - 1, 5));
+
+        System.out.println(binarySearch2(arr, 0, arr.length - 1, 5));
     }
 
     //二分查找
@@ -24,6 +28,37 @@ public class BinarySearch {
             return binarySearch(arr, mid + 1, right, value);
         } else {
             return mid;  //表示arr[mid]==value，找到该数，返回下标
+        }
+    }
+
+    //用二分查找返回  所有等于value的值的下标
+    public static ArrayList<Integer> binarySearch2(int[] arr, int left, int right, int value) {
+        if (left > right) {
+            return new ArrayList<Integer>();
+        }
+        int mid = (left + right) / 2;
+        if (arr[mid] > value) {
+            return binarySearch2(arr, left, mid - 1, value);
+        } else if (arr[mid] < value) {
+            return binarySearch2(arr, mid + 1, right, value);
+        } else {
+            ArrayList<Integer> list = new ArrayList<>(); //创建一个集合用于存储所有满足条件的下标
+            int l = mid;  //从当前满足条件的mid下标向左遍历，找到等于value的所有下标
+            while (l >= 0 && arr[l] == value) {
+                l--;
+                if (arr[l] == value) {
+                    list.add(l);
+                }
+            }
+            list.add(mid); //将满足条件的mid下标加入集合
+            int r = mid;  //从当前满足条件的mid下标向右遍历，找到等于value的所有下标
+            while (r <= right && arr[r] == value) {
+                r++;
+                if (arr[r] == value) {
+                    list.add(r);
+                }
+            }
+            return list;
         }
     }
 }
