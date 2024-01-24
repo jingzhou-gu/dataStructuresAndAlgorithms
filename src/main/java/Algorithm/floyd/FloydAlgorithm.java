9a66c2b785e49ff5542241762f43f3e03993af1f
gujingzhou <gjz960420@163.com>
2024-01-24 18:42:55 +0800

    弗洛伊德算法

---
+++ src/main/java/Algorithm/floyd/FloydAlgorithm.java
@@ -0,0 +1,59 @@
+package main.java.Algorithm.floyd;
+
+import java.util.Arrays;
+
+/**
+ * @Author: gjz
+ * @Date: 2024/1/23 18:13
+ * @Description: TODO 弗洛伊德算法
+ */
+public class FloydAlgorithm {
+    public static void main(String[] args) {
+        char[] vertex = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+        final int N = 65535;
+        int[][] matrix = {
+                {0, 5, 7, N, N, N, 2},
+                {5, 0, N, 9, N, N, 3},
+                {7, N, 0, N, 8, N, N},
+                {N, 9, N, 0, N, 4, N},
+                {N, N, 8, N, 0, 5, 4},
+                {N, N, N, 4, 5, 0, 6},
+                {2, 3, N, N, 4, 6, 0}
+        };
+        Graph graph = new Graph(vertex.length, vertex, matrix);
+        graph.show();
+    }
+}
+
+class Graph {
+    char[] vertex; //顶点数组
+    int[][] dis; //各顶点到其他顶点的距离
+    int[][] pre; //到达目标顶点的前驱顶点
+
+    public Graph(int length, char[] vertex, int[][] matrix) {
+        this.vertex = vertex;
+        this.dis = matrix;
+        this.pre = new int[length][length];
+
+        for (int i = 0; i < length; i++) {
+            Arrays.fill(pre[i], i);
+        }
+    }
+
+    public void show() {
+        char[] vertex = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+        for (int i = 0; i < dis.length; i++) {
+            //输出pre数组的一行
+            for (int j = 0; j < dis.length; j++) {
+                System.out.print(vertex[pre[i][j]] + " ");
+            }
+            System.out.println();
+            //输出dis数组的一行
+            for (int j = 0; j < dis.length; j++) {
+                System.out.print("(" + vertex[i] + "到" + vertex[j] + "的最短路径为" + dis[i][j] + ") ");
+            }
+            System.out.println();
+            System.out.println();
+        }
+    }
+}
